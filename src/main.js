54e0f0567b24aff613107dd038d0f71d20aec84e
Muqimjon Mamadaliyev <muqimjon.mr@gmail.com>
2024-03-23 12:44:13 +0500

    Fixed bug and concatination modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import { addClickListener, create, get, set } from "./domApi";
-
 class ItemGenerator {
     createItem(text, index) {
         const listItem = create("li");
@@ -91,3 +89,51 @@ renderToDos();
 
 addClickListener("#add-btn", addNewItem);
 addClickListener(13, addNewItem);
+
+
+// Another module
+
+function get(selector) {
+    return document.querySelector(selector);
+}
+
+function set(parent, child) {
+    parent.appendChild(child);
+}
+
+function create(element){
+    return document.createElement(element)
+}
+
+function addClickListener(key, fn){
+    if(typeof key == "string")
+        get(key).addEventListener("click", fn);
+    else
+        document.addEventListener("keydown", 
+            function(event) { 
+                if (event.keyCode === key) 
+                    fn();
+        });
+}
+
+Array.prototype.newPop = function(index){
+    if(index == undefined)
+        index = this.length - 1
+
+    let item = this[index];
+    this.splice(index, 1);
+    return item;
+}
+
+String.prototype.capitalize = function (){
+    let text = new String();
+    
+    for(let word of this.split(' '))
+        text.add(word ? ' ' + word.title() : ' ');
+
+    return text;
+}
+
+String.prototype.title = function (){
+    return this[0].toUpperCase() + this.slice(1).toLowerCase();
+}
